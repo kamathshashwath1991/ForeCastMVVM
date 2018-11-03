9119bd026f58093c7d7cec57a253a45c2a2bcbe7
Shashwath Kamath <kamathshashwath1991@gmail.com>
2018-11-03 16:39:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kamathshashwath/forecastmvvm/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kamathshashwath.forecastmvvm.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kamathshashwath.forecastmvvm.R
+
+class MainActivity : AppCompatActivity {
+    constructor() : super()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
